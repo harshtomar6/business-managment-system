869019205461ffc3edd5a5ee21a83f5123769dcb
Harsh Tomar <harshtomar6@gmail.com>
2017-11-14 00:39:40 +0530

    Added Sidebar

--- src/controllers/script.js
+++ src/controllers/script.js
@@ -3,5 +3,31 @@
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
 
+  $('#sidebar ul li').click(sideClick);
 })
+
+function toggleSidebar(){
+
+  if($('#sidebar').width() == 0)
+    $('#sidebar').css('width', '260px');
+  else
+    $('#sidebar').css('width', '0px');
+}
+
+function sideClick(){
+  //add CSS
+  $('#sidebar ul li').removeClass('selected');
+  $(this).addClass('selected');
+
+  var tab = $(this).first().text()
+
+  switch(tab){
+    case 'Home':
+      window.location.href='/'
+      break;
+    case 'New Transaction':
+      window.location.href='/new-transaction'
+      break;
+  }
+}
  
